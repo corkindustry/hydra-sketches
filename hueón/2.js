1ac2a3349147d3023582cc6a13aa654164dd2d65
jonah <falsefriend13@gmail.com>
2021-08-17 12:01:43 -0400

    modify

--- hueón/2.js
+++ hueón/2.js
@@ -1,13 +1,15 @@
 s0.initCam(1)
 lfo = () => Math.sin(time)
 n = 50
-a = () => shape(3, 0.29, 0).modulateRotate(s0, 1).mask(osc(n*6, 0.03).modulateRotate(s0).thresh())
+a = () => shape(3, 0.29, 0.01).modulateRotate(s0, 1).mask(osc(n*6, 0.03).modulateRotate(s0).thresh())
 
 a().color(1, 0, 0).out(o0)
 a().color(0, 1, 0).scrollX(1/n/3).out(o1)
-a().color(0, 0, 1).scrollX(2/n/3).out(o2)
+a().color(0, 0, 1).scrollX(2/n/2).out(o2)
 
 // src(o0).add(o1).out(o3)
-solid().add(src(o0),1).add(src(o1),1).add(src(o2),1).scale(1, 1, window.innerWidth/window.innerHeight).out(o3)
+solid().add(src(o0),1).add(src(o1),1).add(src(o2),1)
+// .scale(1, 1, window.innerWidth/window.innerHeight)
+.out(o3)
 
 render(o3)
